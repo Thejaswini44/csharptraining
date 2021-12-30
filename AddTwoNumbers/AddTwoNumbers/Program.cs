05ed8b483c87ba153fddf166010ffc01ef73d941
GaneshIT <ganesh.ceoit@gmail.com>
2021-12-30 13:54:35 +0530

    files added

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+            public static double AddAndMultiply(double a, double b, double c)
+            {
+                return (a + b) * c;
+            }
+            public static void Main()
+            {
+                Console.WriteLine(AddAndMultiply(3, 6, 35));
+                Console.WriteLine(AddAndMultiply(-12, 5,17));
+                Console.WriteLine(AddAndMultiply(-40, 50, 60));
+                Console.WriteLine(AddAndMultiply(1.7, 9.9, 0.01));
+            }
+     }  
+    
+}
